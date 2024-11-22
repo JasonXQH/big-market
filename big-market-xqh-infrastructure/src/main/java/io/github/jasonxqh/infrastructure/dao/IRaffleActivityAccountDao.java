0c388ae7a06e98e4df79eaad6fb3f549bd9461c0
JasonXQH <10195501423@stu.ecnu.edu.cn>
2024-11-22 14:27:24 +0800

    "feat: new"

---
+++ big-market-xqh-infrastructure/src/main/java/io/github/jasonxqh/infrastructure/dao/IRaffleActivityAccountDao.java
@@ -0,0 +1,9 @@
+package io.github.jasonxqh.infrastructure.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouterStrategy;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+@DBRouterStrategy(splitTable = true)
+public interface IRaffleActivityAccountDao {
+}
