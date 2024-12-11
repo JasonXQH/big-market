37078723ef56393f078ca78138ba1f5463728ec8
JasonXQH <10195501423@stu.ecnu.edu.cn>
2024-12-11 15:04:57 +0800

    "feat: 加入未跟踪文件"

---
+++ big-market-xqh-infrastructure/src/main/java/io/github/jasonxqh/infrastructure/dao/IUserCreditAccountDao.java
@@ -0,0 +1,12 @@
+package io.github.jasonxqh.infrastructure.dao;
+
+import io.github.jasonxqh.infrastructure.dao.po.award.UserCreditAccount;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IUserCreditAccountDao {
+    int updateAddAmount(UserCreditAccount userCreditAccountReq);
+
+    void insert(UserCreditAccount userCreditAccountReq);
+    //对积分账户的操作
+}
